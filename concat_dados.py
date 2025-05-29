41d221e69d441397fc3e27d2636f1979be9013e0
Kensyto K. Berthaud <69537393+Kensyto@users.noreply.github.com>
2025-05-29 13:47:32 +0000

    resolvendo o primeiro exercicio de concartenaçao

---
+++ concat_dados.py
@@ -0,0 +1,15 @@
+# Solicita o primeiro dado ao usuário
+
+primeiro_dado = input("Por favor, digite o primeiro dado: ")
+
+# Solicita o segundo dado ao usuário
+
+segundo_dado = input("Agora, digite o segundo dado: ")
+
+# Concatena os dois dados em uma única string
+
+dados_concatenados = f"Você digitou: {primeiro_dado} + " " + {segundo_dado}"
+
+# Exibe o resultado da concatenação
+
+print(dados_concatenados)
